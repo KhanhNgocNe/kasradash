a5b7a950f10a3e188e9ad3844a40f424297ce438
KhanhNgocNe <khanhngoc04032000@gmail.com>
2025-05-20 21:39:15 +0700

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
